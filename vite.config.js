cbeecb5cd7133198653eb5ae7c5aabd23104a8de
Deepak Chhantyal <deepakchhantyal4156@gmail.com>
2025-09-03 16:08:00 +0545

    fix: Update base URL in Vite config to use relative path

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "https://deepakScript.github.io/portfolio-deepak/",
+  base: "/portfolio-deepak/",
   plugins: [react()],
 })
